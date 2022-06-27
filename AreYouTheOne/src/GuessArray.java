d6d4dc4b9e14719880b7c9593fe6fc8727f4fcd2
clara <clararbalmer@gmail.com>
2022-06-27 16:00:48 -0400

    first commit

---
+++ AreYouTheOne/src/GuessArray.java
@@ -0,0 +1,8 @@
+
+public class GuessArray {
+	//fields
+	
+	
+	//constructor
+	public GuessArray() {}
+}
---
+++ AreYouTheOne/src/ShowGuessTester.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShowGuessTester {
+
+	@Test
+	void test() {
+		//test all showguess methods!
+	}
+
+}
